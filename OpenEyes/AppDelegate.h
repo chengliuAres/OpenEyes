663048034e824075ff32a4c5dfeec9da062cc0c7
AppleCheng <applecheng@applecheng.local>
2016-10-22 20:25:43 +0800

    Initial Commit

---
+++ OpenEyes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenEyes
+//
+//  Created by AppleCheng on 2016/10/22.
+//  Copyright © 2016年 AppleCheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenEyes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenEyes
+//
+//  Created by AppleCheng on 2016/10/22.
+//  Copyright © 2016年 AppleCheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
